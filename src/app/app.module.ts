fbe75094309a8b8131860552731d79c3b6aaba96
Marina <maryna.ivanova@extrawest.com>
2018-04-04 12:05:48 +0300

    created base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
